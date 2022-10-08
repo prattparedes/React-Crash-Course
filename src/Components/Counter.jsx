f5c6f1890cc578092ae2a58612577cec9a13cb46
Pratt <pratt.paredes@gmail.com>
2022-10-08 12:57:33 -0500

    how to use callbacks and state to change object

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,59 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    const [counter, setCounter] = useState(0)
+    const [cart, setCart] = useState({ 
+        item:'apple', 
+        quantity: 0,
+    })
+
+    function addApple() {
+        // 1. Use a callback to get the previous value
+        // 2. Spread out all the properties of the prev state
+        // 3. Only change the property that you need to change
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity + 1,
+        }))
+    }
+
+    function deductApple() {
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity - 1,
+        }))
+    }
+
+    function resetApple() {
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: 0
+        }))
+    }
+    // const [counter, setCounter] = useState(0)
+
+    // function incrementCounter() {
+    //     console.log('incremented')
+    //     setCounter((prevCounter) => prevCounter + 1)
+    // }
+
+    // function decrementCounter() {
+    //     console.log('decremented')
+    //     setCounter((prevCounter) => prevCounter - 1)
+    // }
+
+    // function resetCounter() {
+    //     console.log('reseted')
+    //     setCounter(0)
+    // }
 
     return (
         <>
-            <button onClick={() => setCounter(counter - 1)}>-</button>
-            <span>{counter}</span>
-            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={deductApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+            <button onClick={addApple}>+</button>
             <br />
-            <button onClick={() => setCounter(0)}>Reset Counter</button>
+            <button onClick={resetApple}>Reset Counter</button>
         </>
     )
 }
